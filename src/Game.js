8129d2d572aaae144f7f49d21fc7cbcb71bb6b43
cymbiotica <terry.esposito@gmail.com>
2018-09-02 17:41:56 -0400

    refactor and start

---
+++ src/Game.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "../src/styles/App.css";
+
+class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>Test</div>;
+  }
+}
+
+export default Game;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './styles/index';
+import Game from './Game';
+// import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Game />, document.getElementById('root'));
+// registerServiceWorker();
